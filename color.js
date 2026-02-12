0c27d75daf5deb2db92fffd4137035f09b5ef391
jaedomoreno <62164872+jaedomoreno@users.noreply.github.com>
2026-02-12 17:05:02 -0300

    Add files via upload

---
+++ color.js
@@ -0,0 +1,6 @@
+const exercises = [
+  { name: "Rodillas arriba", color: "#e74c3c" },
+  { name: "Puñetazos", color: "#3498db" },
+  { name: "Talones atrás", color: "#f39c12" },
+  { name: "Jumping Jacks", color: "#2ecc71" }
+];
